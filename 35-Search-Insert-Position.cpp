4651c72973bbe6c66a1ecd52eae63877a9d23222
Sujalkan07 <sujalkansal477@gmail.com>
2026-08-31 23:23:59 +0530

    [35] [Time Beats: 100.00%] [Memory Beats: 98.65%] - LeetPush

---
+++ 35-Search-Insert-Position.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start =0 ; int middle;
+        int end = nums.size()-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            middle=mid;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        if(target>nums[middle]){
+            return middle+1;
+        }
+        else {
+            if(middle==0){
+            return 0;
+            }
+            else{
+                return middle;
+            }
+        }
+    }
+};
