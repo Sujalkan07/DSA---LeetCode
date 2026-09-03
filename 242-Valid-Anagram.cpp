2648dd5f02055587c0b761234a368618bf0fc35a
Sujalkan07 <sujalkansal477@gmail.com>
2026-09-04 00:00:51 +0530

    [242] [Time Beats: 100.00%] [Memory Beats: 25.88%] - LeetPush

--- 242-Valid-Anagram.cpp
+++ 242-Valid-Anagram.cpp
@@ -3,12 +3,10 @@ public:
     bool isAnagram(string s, string t) {
         vector<int> vec(26,0);
         for(int i=0;i<s.length();i++){
-            int idx=s[i]-'a';
-            vec[idx]++;
+            vec[s[i]-'a']++;
         }
         for(int i=0;i<t.length();i++){
-            int idx=t[i]-'a';
-            vec[idx]--;
+            vec[t[i]-'a']--;
         }
         for(int i=0;i<26;i++){
             if(vec[i]!=0){
